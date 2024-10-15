c5fa698dbeda8136114334aa2838c0b6bd427a9e
JoaoPLF <joaoplfonseca@hotmail.com>
2024-10-15 15:52:15 -0400

    Initial implementation of speech recognition

--- modules/speech-recognition/android/src/main/java/expo/modules/speechrecognition/SpeechRecognitionModule.kt
+++ modules/speech-recognition/android/src/main/java/expo/modules/speechrecognition/SpeechRecognitionModule.kt
@@ -1,30 +1,87 @@
 package expo.modules.speechrecognition
 
+import android.content.Intent
+import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import expo.modules.kotlin.functions.Queues
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import java.util.Locale
 
 class SpeechRecognitionModule : Module() {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
+  private var speechRecognizer: SpeechRecognizer? = null
+  private val recognitionListener: RecognitionListener
+  private var response: ArrayList<String>? = null
+
+  init {
+    recognitionListener = object : RecognitionListener {
+      override fun onReadyForSpeech(params: Bundle?) {
+        TODO("Not yet implemented")
+      }
+
+      override fun onBeginningOfSpeech() {
+        TODO("Not yet implemented")
+      }
+
+      override fun onRmsChanged(rmsdB: Float) {
+        TODO("Not yet implemented")
+      }
+
+      override fun onBufferReceived(buffer: ByteArray?) {
+        TODO("Not yet implemented")
+      }
+
+      override fun onEndOfSpeech() {
+        TODO("Not yet implemented")
+      }
+
+      override fun onError(error: Int) {
+        TODO("Not yet implemented")
+      }
+
+      override fun onResults(results: Bundle?) {
+        TODO("Not yet implemented")
+      }
+
+      override fun onPartialResults(partialResults: Bundle?) {
+        TODO("Not yet implemented")
+      }
+
+      override fun onEvent(eventType: Int, params: Bundle?) {
+        TODO("Not yet implemented")
+      }
+
+    }
+  }
+
   override fun definition() = ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('SpeechRecognition')` in JavaScript.
     Name("SpeechRecognition")
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants(
-      "PI" to Math.PI
-    )
-
-    // Defines event names that the module can send to JavaScript.
     Events("onChange")
 
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      "Hello world! 👋"
-    }
+    AsyncFunction("startSpeechRecognition") {
+      speechRecognizer = SpeechRecognizer.createSpeechRecognizer(appContext.reactContext)
+
+      val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+        putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.US)
+      }
+
+      speechRecognizer?.setRecognitionListener(recognitionListener)
+      speechRecognizer?.startListening(intent)
+
+      sendEvent("OK")
+    }.runOnQueue(Queues.MAIN)
+
+    AsyncFunction("stopSpeechRecognition") {
+      speechRecognizer?.stopListening()
+      speechRecognizer?.destroy()
+      speechRecognizer = null
+
+      sendEvent("OK")
+    }.runOnQueue(Queues.MAIN)
 
     // Defines a JavaScript function that always returns a Promise and whose native code
     // is by default dispatched on the different thread than the JavaScript runtime runs on.
@@ -33,15 +90,6 @@ class SpeechRecognitionModule : Module() {
       sendEvent("onChange", mapOf(
         "value" to value
       ))
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
-    View(SpeechRecognitionView::class) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { view: SpeechRecognitionView, prop: String ->
-        println(prop)
-      }
     }
   }
 }
